1a9bf9e87d02ab435f718d221f30c4facac0874c
unknown <mohdarif013579@gmail.com>
2023-12-06 16:39:10 +0530

    lesson 1

---
+++ theoryWithExamples/lesson.js
@@ -0,0 +1,34 @@
+// What is typescript ?
+
+// A javascript Superset
+// A language  building upon js
+// Add new features + Advantages to js
+// Browser Can't Execute it
+
+// Ts Compiled to js
+
+// How can typescript add new features ? if what you get in the end is regular js ?
+
+// ts compiler these new features to js workarounds
+
+// Why typescript ?
+
+// Suppose you write a program add two numbers ?
+
+function addNumbers(a, b) {
+  return a + b;
+}
+
+console.log(addNumbers("2", "3"));
+
+// This is error because the arguments you passed into the function is treat like string 
+// In js if you pass string it should be treated as concent string  the output should be 23 
+
+// You can solve this above problem by using the typescript 
+// unwanted behaviour at runtime
+// > mitigration strategies {
+ // add if check to add functions validation and sanitze uer inputes
+//}
+//> Developers can still write invalid code 
+// typescript is a tool that helos developers write the better codes
+
