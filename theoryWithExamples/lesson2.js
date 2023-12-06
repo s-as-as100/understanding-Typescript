2a5e084e710315c304eb5835122574d12ef1f280
unknown <mohdarif013579@gmail.com>
2023-12-06 19:55:38 +0530

    project setup

---
+++ theoryWithExamples/lesson2.js
@@ -0,0 +1,7 @@
+// Advantage of Typescript
+// 1. Types
+// 2. Next Generation features (compiled down for older versions)
+// 3. Non- js features like interfaces or generics
+// 4. Met Programming features like decorators ( Explain soon)
+// 5.  Rich Compiler Option ( u can side loose type or stric type)
+// 6. Modern Tooling that helps even in typescript projects
